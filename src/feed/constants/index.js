2c6820d8697678ebd03bc8b556bfa5377eabcf8a
Michael Chiou <michael.chiou@gamgee.com>
2021-03-05 17:01:53 +0100

    task/STR-03 Replaced xml parsing and user of rss with json instead

--- src/feed/constants/index.js
+++ src/feed/constants/index.js
@@ -8,3 +8,7 @@ export const UPDATE_FEED = 'UPDATE_FEED';
 
 // Helpers
 export const FIRST = 0;
+
+// URLS
+export const BASE_URL = 'https://www.reddit.com';
+export const USER_URL = 'https://www.reddit.com/user';
